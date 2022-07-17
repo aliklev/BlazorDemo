4c20df4192e0f132a3531f56d377c9d979565b0c
Alik <dj_alik@yahoo.com>
2022-07-17 08:33:39 +0300

    winforms demo

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -2,6 +2,7 @@ using DemoApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using SharedLibrary;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
--- WinFormsApp/Form1.cs
+++ WinFormsApp/Form1.cs
@@ -1,3 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using SharedLibrary;
+using SharedLibrary.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace WinFormsApp
 {
     public partial class Form1 : Form
@@ -5,6 +21,24 @@ namespace WinFormsApp
         public Form1()
         {
             InitializeComponent();
+
+            var services = new ServiceCollection();
+            services.AddMudServices();
+            services.AddWindowsFormsBlazorWebView();
+            
+            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("wwwroot\\\\index.html\"") });
+
+
+            blazorWebView1.HostPage = "wwwroot\\index.html";
+
+
+
+            blazorWebView1.Services = services.BuildServiceProvider();
+
+
+
+            blazorWebView1.RootComponents.Add<App>("#app");
+
         }
     }
 }
