c0d09b2f66c5182bcf18cc1a70eb7137240f6017
Alik <dj_alik@yahoo.com>
2022-07-17 15:01:37 +0300

    maui client added

--- WinFormsApp/Form1.cs
+++ WinFormsApp/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebView;
 using Microsoft.AspNetCore.Components.WebView.WindowsForms;
 using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
@@ -26,18 +27,20 @@ namespace WinFormsApp
             services.AddMudServices();
             services.AddWindowsFormsBlazorWebView();
             
-            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("wwwroot\\\\index.html\"") });
-
+            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost") });
 
             blazorWebView1.HostPage = "wwwroot\\index.html";
-
-
-
             blazorWebView1.Services = services.BuildServiceProvider();
-
-
-
             blazorWebView1.RootComponents.Add<App>("#app");
+
+    //        blazorWebView1.UrlLoading +=
+    //(sender, urlLoadingEventArgs) =>
+    //{
+    //    if (urlLoadingEventArgs.Url.Host != "0.0.0.0")
+    //    {
+    //        urlLoadingEventArgs.UrlLoadingStrategy = UrlLoadingStrategy.OpenInWebView;
+    //    }
+    //};
 
         }
     }
